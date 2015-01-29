8a88edf8b15a151d94593b3087fcb7bb34639b7e
Seth Shober <seth.shober@gmail.com>
2015-01-29 14:01:01 -0800

    readPosts works and grabs only 10 jade files

--- models/posts.js
+++ models/posts.js
@@ -8,23 +8,23 @@ var files = fs.readdirSync(process.cwd() + "/posts");
 var postChunk = [];
 var i = 0;
 var file;
+
     while (postChunk.length < 10) {
 
       if ( files[i].indexOf('.jade') !== -1 ) { 
 
       file = fs.readFileSync( path.join( process.cwd() + "/posts", files[i] ) );
-      postChunk.push( jade.render(file) ); i++; }
-      
-      else if (error) { throw error; }
+      postChunk.push( jade.render(file) ); i++; 
+
+  	  } else if (error) { throw error; }
 
   	}
       
   	console.log(postChunk);
   	return postChunk;
   	
-  	}
-
 }
+
 
 module.exports = {
 	"readPosts": readPosts

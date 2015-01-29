e49399bce60fce8d9bf617eb6b802a4b44c15d89
Seth Shober <seth.shober@gmail.com>
2015-01-29 13:56:31 -0800

    readPosts refactor

--- models/posts.js
+++ models/posts.js
@@ -7,10 +7,14 @@ function readPosts(error) {
 var files = fs.readdirSync(process.cwd() + "/posts");
 var postChunk = [];
 var i = 0;
+var file;
     while (postChunk.length < 10) {
 
-      var file = fs.readFileSync( path.join( process.cwd() + "/posts", files[i] ) );
-      if ( file.indexOf('.jade') !== -1 ) { postChunk.push( jade.render(file) ); i++; }
+      if ( files[i].indexOf('.jade') !== -1 ) { 
+
+      file = fs.readFileSync( path.join( process.cwd() + "/posts", files[i] ) );
+      postChunk.push( jade.render(file) ); i++; }
+      
       else if (error) { throw error; }
 
   	}
